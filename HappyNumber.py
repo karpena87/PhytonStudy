54082ca2af230aab6a86ab5e01f6e8a8489a264b
Maria <karpen.masha@gmail.com>
2024-02-29 09:12:00 +0300

    Решение задачи Счастливые числа

---
+++ HappyNumber.py
@@ -0,0 +1,20 @@
+#перебрать числа по порядку до n-1
+#находить остаток от деления n на каждое число
+#если остаток от деления 0 - то прибавить к сумме
+#если сумма итого равна n - выводить True
+#счастливые числа 6 28 496 8128
+
+n = int(input("Введите любое число "))
+
+def is_happy_number(n):
+    s=0
+    for a in range (1,n-1):
+        if n % a == 0:
+            s = s + a
+    if n==s:
+        return(True)
+    else:
+        return(False)
+
+result = is_happy_number(n)
+print(result)
