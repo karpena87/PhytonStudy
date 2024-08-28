3775d6baac3f3986c94739b3f204d43b9b778501
Maria <karpen.masha@gmail.com>
2024-08-28 09:32:54 +0300

    ChatBot on streamlit witch can request to LLM

--- ChatBotGigaChat.py
+++ ChatBotGigaChat.py
@@ -3,8 +3,9 @@ import requests
 import json
 import urllib3
 
-urllib3.disable_warnings()
+urllib3.disable_warnings() # отклбчение варнингов связанных с сертификатами и доступами
 
+# Обращение за access-токеном
 url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
 payload='scope=GIGACHAT_API_PERS'
 headers = {
@@ -15,11 +16,11 @@ headers = {
 }
 
 token = requests.request("POST", url, verify=False, headers=headers, data=payload)
-#print(token.text)
 
+# Извлечение из json значения ключа 'access_token' для авторизации запроса к GigaChat
 body_dict = token.json()
 giga_chat = body_dict['access_token']
-#print(giga_chat)
+
 
 st.title("Чат-бот Copilot")
 
@@ -32,7 +33,7 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Поле для ввода пользовательского запроса
+# Ввод пользовательского запроса
 if prompt := st.chat_input("Введите ваш запрос"):
     # Вывод сообщения пользователя в контейнере сообщений
     with st.chat_message("Пользователь"):
@@ -40,7 +41,7 @@ if prompt := st.chat_input("Введите ваш запрос"):
     # Добавление пользовательского сообщения в историю чата
     st.session_state.messages.append({"role": "Пользователь", "content": prompt})
 
-    #Запрос к GigaChat
+    # Запрос к GigaChat
     url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
 
     payload = json.dumps({
@@ -48,11 +49,11 @@ if prompt := st.chat_input("Введите ваш запрос"):
         "messages": [
             {
                 "role": "user",
-                "content": prompt
+                "content": prompt #'Ты - системный аналитик, который отвечает на вопросы пользователей. Отвечай на вопрос кратко и по существу, опирайся на предоставленный контекст. Контекст:' + prompt
             }
         ],
-        "temperature": 1,
-        "top_p": 0.1,
+        "temperature": 0.87,
+        "top_p": 0.47,
         "n": 1,
         "stream": False,
         "max_tokens": 512,
@@ -66,13 +67,13 @@ if prompt := st.chat_input("Введите ваш запрос"):
 
     response = requests.request("POST", url, verify=False, headers=headers, data=payload)
 
-    #body_dict = response.json()
-    #response_giga_chat = body_dict['content']
+    # Извлечение из json непосредственно сообщения
+    results = json.loads(response.text)
+    content_ai = results['choices'][0]['message']['content']
 
     # Вывод ответа ассистента в контейнере сообщений
     with st.chat_message("Ассистент"):
-        st.markdown(response.text)
-
+        st.markdown(content_ai)
 
     # Добавление ответа ассистента в историю чата
-    st.session_state.messages.append({"role": "Ассистент ", "content": response.text})
+    st.session_state.messages.append({"role": "Ассистент ", "content": content_ai})
